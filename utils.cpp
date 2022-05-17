6b1d79233b42fb5c767a42523cec5a9a884e9144
Alex Kong <akong@y-intercept.net>
2022-05-17 12:39:44 +0800

    sha256 test

---
+++ utils.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+#include "utils.h"
+
+using namespace std;
+
+string bytes_to_hex_string(const unsigned char* input, size_t input_len) {
+
+  stringstream ss;
+
+  ss << hex;
+  for(int i = 0; i < input_len; i++) {
+      ss << hex << setw(2) << std::setfill('0')<< (int)input[i];
+  }
+  return ss.str();
+}
+
+void hex_string_to_bytes(const string& hex_input, unsigned char* output) {
+
+  string hex_input_ = hex_input;
+
+  if (hex_input_.length() % 2 != 0) {
+      hex_input_.insert(0, "0");
+  }
+  
+  for (int i = 0; i < hex_input_.length(); i += 2) {
+    output[i/2] = strtol(hex_input_.substr(i, 2).c_str(), NULL, 16);
+  }
+}
---
+++ utils.h
@@ -0,0 +1,23 @@
+#ifndef CRYPTO_UTILS_H
+#define CRYPTO_UTILS_H
+
+#include <assert.h>
+#include <limits.h>
+
+static_assert (CHAR_BIT == 8);
+
+using namespace std;
+/*
+ * @brief Convert a bytes array to a hex std::string
+ * @param input A pointer to an array of bytes.
+ * @param input_len length of input
+ */
+string bytes_to_hex_string(const unsigned char* input, size_t input_len);
+/*
+ * @brief Convert a hex std::string to an array of bytes
+ * @param hex_input A std:string containing the hex-encoded string.
+ * @param output: a pointer to an array of bytes converted from hex_input
+ */
+void hex_string_to_bytes(const string& hex_input, unsigned char* output);
+
+#endif
