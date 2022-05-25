5d80d063e6d46fec5ec20af9f7db3f3ae3ef9233
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-05-25 16:44:37 +0800

    update

--- test/base/test-base64.cpp
+++ test/base/test-base64.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main() {
   unsigned char input[] = {0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x21, 0x21, 0x21};
   char* output;
-  size_t* output_len = (size_t*)malloc(sizeof(size_t));
+  size_t* output_len;
   output = encode_bytes_to_base64_string(input, sizeof(input), output_len);
-  for (int i = 0; i < *output_len-1; i++) {
+  for (int i = 0; i < *output_len; i++) {
     cout << output[i];
   }
   cout << endl;
