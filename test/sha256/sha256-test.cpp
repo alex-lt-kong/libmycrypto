d138f890979e128312893ae30a9e52836ea06e01
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-06-08 14:50:44 +0800

    make repo C only

--- test/sha256/sha256-test.cpp
+++ test/sha256/sha256-test.cpp
@@ -19,6 +19,7 @@ void test_rsp_file(string rsp_file_dir) {
   string line;
   unsigned char sha_hash[SHA256_HASH_SIZE];
   size_t next_msg_len = -1;
+  char md_char[SHA256_HASH_SIZE];
   while (getline(infile, line)) {
     if (line.length() >= 3 && line.substr(0, 3) == "Len") {
       next_msg_len = stoi(line.substr(6, line.length() - 6 - 1));
@@ -27,13 +28,15 @@ void test_rsp_file(string rsp_file_dir) {
       string msg = line.substr(6, line.length() - 6 - 1);
       assert (msg.length() % 2 == 0);
       cout << "Msg = " << msg.substr(0, 128) << (msg.length() > 128 ? "...[Truncated]" : "" ) << "\n";
-      unsigned char msg_bytes[next_msg_len / CHAR_BIT];      
-      hex_string_to_bytes(msg, msg_bytes);
-      cal_sha256_hash(msg_bytes, next_msg_len / CHAR_BIT, sha_hash);
-      cout << "MD = " << bytes_to_hex_string(sha_hash, SHA256_HASH_SIZE) << " ";
+      unsigned char msg_bytes[next_msg_len / CHAR_BIT];            
+      HexToBin(msg.c_str(), msg_bytes, strlen(msg.c_str()));
+      cal_sha256_hash(msg_bytes, next_msg_len / CHAR_BIT, sha_hash);     
+      
+      BinToHex(sha_hash, SHA256_HASH_SIZE, md_char, SHA256_HASH_SIZE * 2 + 1, 0);
+      printf("MD = %s ", md_char);
     } else if (line.length() >= 2 && line.substr(0, 2) == "MD") {
       string official_md = line.substr(5, line.length() - 5 - 1);
-      if (bytes_to_hex_string(sha_hash, SHA256_HASH_SIZE) == official_md) {
+      if (strcmp(md_char, official_md.c_str()) == 0) {
         cout << "Passed" << endl;
       } else {
         cout << "====== FAILED ======, expecting " << official_md << endl;
