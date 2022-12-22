cda0d6ff1531719b0cdc695f44601baf79168468
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-12-22 14:01:17 +0800

    goto to handle exception

--- tools/base64-decode.c
+++ tools/base64-decode.c
@@ -9,14 +9,17 @@ int main (int argc, char *argv[]) {
         fprintf(stderr, "Usage: <src filepath> <dest filepath>\n");
         return 1;
     }
-    FILE* fp_in;
-    uint8_t* buffer;
+    FILE* fp_in = NULL;
+    FILE *fp_out = NULL;
+    uint8_t* buffer = NULL;
+    uint8_t* bytes_out = NULL;
     size_t file_len;
-
+    size_t retval = 0;
     fp_in = fopen(argv[1], "rb");          // Open the file in binary mode
     if (fp_in == NULL) {
         fprintf(stderr, "fopen() failed\n");
-        return 1;
+        retval = 1;
+        goto finally;
     }
     fseek(fp_in, 0, SEEK_END);          // Jump to the end of the file
     file_len = ftell(fp_in);             // Get the current byte offset in the file
@@ -25,33 +28,44 @@ int main (int argc, char *argv[]) {
  
     if (NULL == buffer) {
         fprintf(stderr, "malloc() failed\n");
-        fclose(fp_in);
-        return 1;
+        retval = 1;
+        goto finally;
     }
     fread(buffer, file_len, 1, fp_in);
     size_t output_len;
-    uint8_t* bytes_out;
-    bytes_out = decode_base64_string_to_bytes(buffer, &output_len);
+    
+    bytes_out = decode_base64_string_to_bytes((char*)buffer, &output_len);
     if (NULL == bytes_out) {
-        fprintf(stderr, "malloc() failed\n");
-        fclose(fp_in);
-        free(buffer);
-        return 1;
+        fprintf(stderr, "decode_base64_string_to_bytes() failed, most likely due to malloc() failure\n");
+        retval = 1;
+        goto finally;
     }
-    FILE *fp_out;
+    
 
-    fp_out = fopen(argv[2],"wb");  // w for write, b for binary
+    fp_out = fopen(argv[2], "wb");
     if (fp_out == NULL) {
         fprintf(stderr, "fopen() failed\n");
-        fclose(fp_in);
-        free(buffer);
-        free(bytes_out);
-        return 1;
+        retval = 1;
+        goto finally;
     }
-    fwrite(bytes_out, output_len, 1, fp_out); // write 10 bytes from our buffer
-    fclose(fp_out);
-    fclose(fp_in);
-    free(buffer);
-    free(bytes_out);
-    return 0;
+    size_t bytes_written = fwrite(bytes_out, sizeof(uint8_t), output_len, fp_out);
+    if (bytes_written < output_len) {
+        fprintf(stderr, "fwrite() failed, expecting %lu bytes but write %lu bytes only\n", output_len, bytes_written);
+        retval = 1;
+        goto finally;
+    }
+finally:
+    if (fp_out != NULL) {
+        fclose(fp_out);
+    }
+    if (fp_in != NULL) {
+        fclose(fp_in);
+    }
+    if (buffer != NULL) {
+        free(buffer);
+    }
+    if (bytes_out != NULL) {
+        free(bytes_out);
+    }
+    return retval;
 }
--- tools/base64-encode.c
+++ tools/base64-encode.c
@@ -9,14 +9,17 @@ int main (int argc, char *argv[]) {
         fprintf(stderr, "Usage: base64-encode <file_path>\n");
         return 1;
     }
-    FILE *fp;
+    FILE *fp = NULL;
     uint8_t *buffer;
     long file_len;
+    size_t retval = 0;
+    char* b64_str = NULL;
 
     fp = fopen(argv[1], "rb");
     if (fp == NULL) {
         fprintf(stderr, "fopen() failed\n");
-        return 1;
+        retval = 1;
+        goto finally;
     }
     fseek(fp, 0, SEEK_END);          // Jump to the end of the file
     file_len = ftell(fp);             // Get the current byte offset in the file
@@ -24,19 +27,24 @@ int main (int argc, char *argv[]) {
     buffer = malloc(file_len * sizeof(uint8_t));
     if (buffer == NULL) {
         fprintf(stderr, "malloc() failed\n");
-        fclose(fp);
-        return 1;
+        retval = 1;
+        goto finally;
     }
-    printf("reading %u bytes into memory...\n", file_len);
+    fprintf(stderr, "reading %lu bytes into memory...\n", file_len);
     fread(buffer, file_len, 1, fp); // Read in the entire file
-    char* b64_str = encode_bytes_to_base64_string(buffer, file_len);
+    b64_str = encode_bytes_to_base64_string(buffer, file_len);
     if (b64_str == NULL) {
-        fprintf(stderr, "malloc() failed\n");
-        fclose(fp);
-        return 1;
+        fprintf(stderr, "encode_bytes_to_base64_string() failed, most likely because of malloc() failure\n");        
+        retval = 1;
+        
     }
     printf("%s\n", b64_str);
-    free(b64_str);
-    fclose(fp); // Close the file
-    return 0;
+finally:
+    if (b64_str != NULL) {
+        free(b64_str);
+    }
+    if (fp != NULL) {
+        fclose(fp);
+    }
+    return retval;
 }
